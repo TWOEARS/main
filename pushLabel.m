a2824b197c1887ad24e6f393aaeffee41e69ed42
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-07-14 18:50:44 +0200

    * bugfix with empty onset list * fastened onset interpolation by using 441 Hz as interpolation rate

--- pushLabel.m
+++ pushLabel.m
@@ -61,80 +61,86 @@ if handles.l < 0  &&  l < 0
 end
 
 if onoffsChanged
-    handles.onsets{end} = sort( handles.onsets{end} );
-    handles.offsets{end} = sort( handles.offsets{end} );
-    i = 2;
-    while i <= length( handles.onsets{end} )
-        if handles.onsets{end}(i) <= handles.offsets{end}(i-1) + 1
-            handles.offsets{end}(i-1) = handles.offsets{end}(i);
-            handles.onsets{end}(i) = [];
-            handles.offsets{end}(i) = [];
-        else
-            i = i + 1;
-        end
-    end
-    
-    onoffs = [];
-    for j = 1:length( handles.onsets )
-        for i = 1:length( handles.onsets{j} )
-            onset = handles.onsets{j}(i);
-            offset = handles.offsets{j}(i);
-            if isnan(onset) || isnan(offset)
-                continue;
-            end
-            onoffs = [onoffs; onset, +1, 1; offset, -1, 1];
-        end
-    end
-    onoffs = sortrows( onoffs, 1 );
-    i = 1;
-    while i < size( onoffs, 1 )
-        if sign( onoffs(i,2) ) == sign( onoffs(i+1,2) )
-            if onoffs(i,3) == 1
-                onoffs(i+1,2) = onoffs(i,2) + onoffs(i+1,2);
-                onoffs(i+1,3) = max( 1, onoffs(i+1,1) - onoffs(i,1) );
-                onoffs(i,:) = [];
+    if ~isempty( handles.onsets )
+        handles.onsets{end} = sort( handles.onsets{end} );
+        handles.offsets{end} = sort( handles.offsets{end} );
+        i = 2;
+        while i <= length( handles.onsets{end} )
+            if handles.onsets{end}(i) <= handles.offsets{end}(i-1) + 1
+                handles.offsets{end}(i-1) = handles.offsets{end}(i);
+                handles.onsets{end}(i) = [];
+                handles.offsets{end}(i) = [];
             else
-                onoffs(i+1,3) = max( 1, onoffs(i+1,1) - onoffs(i,1) );
                 i = i + 1;
             end
-        else
-            i = i + 1;
         end
-    end
-    s = 0;
-    levels = [];
-    for i = 1:size( onoffs, 1 )
-        if onoffs(i,2) > 0
-            levels = [levels; onoffs(i,1) - onoffs(i,3), s];
-            s = s + onoffs(i,2);
-            levels = [levels; onoffs(i,1), s];
-        else
-            levels = [levels; onoffs(i,1) - onoffs(i,3), s];
-            s = s + onoffs(i,2);
-            levels = [levels; onoffs(i,1), s];
+        
+        onoffs = [];
+        for j = 1:length( handles.onsets )
+            for i = 1:length( handles.onsets{j} )
+                onset = handles.onsets{j}(i);
+                offset = handles.offsets{j}(i);
+                if isnan(onset) || isnan(offset)
+                    continue;
+                end
+                onoffs = [onoffs; onset, +1, 1; offset, -1, 1];
+            end
         end
-    end
-    levels = [0,0; levels; length( handles.s ) + 1,0];
-    i = size( levels, 1 ) - 1;
-    while i > 0
-        if levels(i,1) == levels(i+1,1)
-            levels(i,:) = [];
-        else
-            i = i - 1;
+        onoffs = sortrows( onoffs, 1 );
+        i = 1;
+        while i < size( onoffs, 1 )
+            if sign( onoffs(i,2) ) == sign( onoffs(i+1,2) )
+                if onoffs(i,3) == 1
+                    onoffs(i+1,2) = onoffs(i,2) + onoffs(i+1,2);
+                    onoffs(i+1,3) = max( 1, onoffs(i+1,1) - onoffs(i,1) );
+                    onoffs(i,:) = [];
+                else
+                    onoffs(i+1,3) = max( 1, onoffs(i+1,1) - onoffs(i,1) );
+                    i = i + 1;
+                end
+            else
+                i = i + 1;
+            end
         end
+        s = 0;
+        levels = [];
+        for i = 1:size( onoffs, 1 )
+            if onoffs(i,2) > 0
+                levels = [levels; onoffs(i,1) - onoffs(i,3), s];
+                s = s + onoffs(i,2);
+                levels = [levels; onoffs(i,1), s];
+            else
+                levels = [levels; onoffs(i,1) - onoffs(i,3), s];
+                s = s + onoffs(i,2);
+                levels = [levels; onoffs(i,1), s];
+            end
+        end
+        levels = [0,0; levels; length( handles.s ) + 1,0];
+        i = size( levels, 1 ) - 1;
+        while i > 0
+            if levels(i,1) == levels(i+1,1)
+                levels(i,:) = [];
+            else
+                i = i - 1;
+            end
+        end
+        levelsMax = max( levels(:,2) );
+        levels(:,2) = levels(:,2) ./ levelsMax;
+        interpFsFactor = floor( handles.fs / 441 );
+        levelsInterp = interp1( levels(:,1), levels(:,2), 1:interpFsFactor:length( handles.s ) );
+        winSize = 0.2 * 441;
+        levelsInterp = filter( pdf( 'Normal', -floor(winSize/2):floor(winSize/2), 0, floor(winSize/8) ), 1, [levelsInterp zeros(1,ceil(winSize/2))] );
+        levelsInterp = [levelsInterp(ceil(winSize/2):end) zeros(1,ceil(winSize))];
+        lmin = 1 / 3; %(1 / levelsMax) + (1 - (1 / levelsMax) ) / 3;
+        levelsInterp(levelsInterp < lmin) = 0;
+        levelsInterp(levelsInterp >= lmin) = 1;
+        levelsInterpDelta = [levelsInterp, 0] - [0, levelsInterp];
+        handles.onsetsInterp = find( levelsInterpDelta == +1 ) .* interpFsFactor;
+        handles.offsetsInterp = find( levelsInterpDelta == -1 ) .* interpFsFactor;
+    else
+        handles.onsetsInterp = [];
+        handles.offsetsInterp = [];
     end
-    levelsMax = max( levels(:,2) );
-    levels(:,2) = levels(:,2) ./ levelsMax;
-    levelsInterp = interp1( levels(:,1), levels(:,2), 1:length( handles.s ) );
-    winSize = 0.2 * handles.fs;
-    levelsInterp = filter( pdf( 'Normal', -floor(winSize/2):floor(winSize/2), 0, floor(winSize/8) ), 1, [levelsInterp zeros(1,ceil(winSize/2))] );
-    levelsInterp = [levelsInterp(ceil(winSize/2):end) zeros(1,ceil(winSize))];
-    lmin = 1 / 3; %(1 / levelsMax) + (1 - (1 / levelsMax) ) / 3;
-    levelsInterp(levelsInterp < lmin) = 0;
-    levelsInterp(levelsInterp >= lmin) = 1;
-    levelsInterpDelta = [levelsInterp, 0] - [0, levelsInterp];
-    handles.onsetsInterp = find( levelsInterpDelta == +1 );
-    handles.offsetsInterp = find( levelsInterpDelta == -1 );
     
     tout = '';
     for i = 1:length(handles.onsets)
