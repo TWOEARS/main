0df37646b5169d789d4eb3d0742d1265781d9428
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-07-25 17:50:36 +0200

    more audioplayer stability code

--- playerStopped.m
+++ playerStopped.m
@@ -5,4 +5,7 @@ if handles.currentKey == handles.preLabelKey
     handles.overrun = true;
 end
 guidata( hfig, handles );
+while( strcmp( hPlayer.running, 'on' ) )
+    sleep( 0.01 );
+end
 play(hPlayer);
