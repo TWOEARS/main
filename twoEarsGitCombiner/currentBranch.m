645083ee440078b0c58e7a7eb8b12a19cb9d6e92
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-09-22 11:48:07 +0200

    GitCombiner: added error message about not being able to determine the current branch.

--- twoEarsGitCombiner/currentBranch.m
+++ twoEarsGitCombiner/currentBranch.m
@@ -5,6 +5,10 @@ cd( repoDir );
 
 branchReturn = git( 'branch' );
 branchSignPos = strfind( branchReturn, '*' );
+if isempty(branchSignPos)
+    error( ['Was not able to determine the current branch. ' ...
+        'Maybe your git binary dir is not included in the system path?'] );
+end
 branchName = sscanf( branchReturn(branchSignPos+1:end), '%s', 1 );
 
 cd( dirTmp );
