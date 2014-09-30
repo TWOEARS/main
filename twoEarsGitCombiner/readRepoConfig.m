9381ce9e30f78f8613e67e7c7daea2ef2a61b7d2
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-09-30 14:41:49 +0200

    Added documentation to readRepoConfig()

--- twoEarsGitCombiner/readRepoConfig.m
+++ twoEarsGitCombiner/readRepoConfig.m
@@ -1,4 +1,8 @@
 function repoConfig = readRepoConfig( rcXmlFileName, tagname )
+%READREPOCONFIG returns the path for the specififed Two!Ears part
+%
+% readRepoConfig( configXmlFile, partName ) returns the path for the Two!Ears
+% software part partName as specified in the configXmlFile.
 
 rcXml = xmlread( rcXmlFileName );
 
