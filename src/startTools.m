24980032be0ff7fe740fc99687010df40aa4bb20
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-10-08 14:08:02 +0200

    Updated start up behavior

--- src/startTools.m
+++ src/startTools.m
@@ -1,11 +1,10 @@
-
 % This script initialises the path variables that are needed for running
 % the Tools code.
 
 basepath = fileparts(mfilename('fullpath'));
+basepath = [basepath filesep];
 
 % Add all relevant folders to the matlab search path
-addpath(fullfile(basepath, 'src'));
-addpath(fullfile(basepath, 'src', filesep, 'args'));
+addpath(fullfile(basepath, 'args'));
 
 clear basepath;
