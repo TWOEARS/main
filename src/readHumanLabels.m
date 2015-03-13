7afe0b1c08a8480a7caf17ebd1881b3fc10f0321
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-03-13 12:57:35 +0100

    Make readHumanLabels work with nested data and add comemnts

--- src/readHumanLabels.m
+++ src/readHumanLabels.m
@@ -1,21 +1,29 @@
 function [humanLabels, stats] = readHumanLabels(labelFile)
 % readHumanLabels returns a cell array containing filenames and human labels
 %
-% Usage: [fileNames,nFiles] = readHumanLabels(labelFile)
+% USAGE:
+%   [humanLabels, stats] = readHumanLabels(labelFile)
 %
-% readFileList(fileList) returns a cell containing all the files stored in the
-% file fileList. A fileList can be created under Linux the following way. Assume
-% we want to have all files in the subdir "sound_databases/database1/training1"
-% listed in the fileList "training1.flist", then run:
-% $ ls -d -1 sound_databases/database1/training1/**/* > training1.flist
-% Note, that you always have to start with a path at the root of the Two!Ears
-% data repository.
+% INPUT PARAMETERS:
+%   labelFile   - csv file storing results from listening tests. ',' is assumed as a
+%                 delimiter and '#' as a comment sign, meaning all lines starting
+%                 with # will be ignored.
 %
-% Dependency: Two!Ears Binaural Simulator
-
-% TODO: expand this function to handle more than one given file list.
-
-% AUTHOR: Hagen Wierstorf
+% OUTPUT PARAMETERS:
+%   humanLabels - cell containing the data from the labelFile
+%   stats       - structure, containing:
+%                   .rows    - number of rows in humanLabels
+%                   .columns - number of columns in humanLabels
+%                   .nested  - number of nested cells
+%
+% DETAILS:
+%   Under the experiment/ folder in the Two!Ears database so called human label files are
+%   stored containing results from listening experiments. The files should be in the
+%   format of csv files using ',' as column delimiters. It can contain numerical as well
+%   as string data. The data is returned as a cell array and can be accessed in the form
+%   humanLabels{1,2}. One entry in the data file can also include nested data in the form
+%   {1, 2}. The corresponding cell in humanLabels will then contain a vector which can be
+%   accsess in the form humanLabels{1,2}(1).
 
 % Checking of input parameters
 narginchk(1,1);
@@ -23,6 +31,20 @@ narginchk(1,1);
 % Get labelFile
 labelFile = xml.dbGetFile(labelFile);
 % Use readtext to get entries fron file
-[humanLabels, stats]= readtext(labelFile, ',', '#', '{}', '');
-
+[humanLabels, tmp]= readtext(labelFile, ',', '#', '{}', '');
+stats.rows = size(humanLabels,1);
+stats.columns = size(humanLabels,2);
+stats.nested = tmp.quote;
+% Look for a string containing '{...}' and convert it to a cell.
+% This is needed for nested data. For example, in the case of localization it could be
+% that the listener perceived 2 instead of 1 source, then we will have two direction
+% nested in a cell.
+for ii = 1:length(humanLabels(:))
+    entry = humanLabels(ii);
+    if iscellstr(entry) && (strcmp(entry{1}(1), '{') && strcmp(entry{1}(end), '}'))
+        % Remove {}-brackets, split string at ',' and convert to double
+        entry = str2double(strsplit(entry{1}(2:end-1),','));
+        humanLabels(ii) = {entry};
+    end
+end
 % vim: set sw=4 ts=4 expandtab textwidth=90 :
