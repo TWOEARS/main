be085ca1d22f57666f3bb9ef540178a14dcf2265
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2015-11-02 16:50:23 +0100

    added cellSqueezeFun.m

---
+++ src/tools/cellSqueezeFun.m
@@ -0,0 +1,33 @@
+function cs = cellSqueezeFun( fn, c, dim )
+
+szC = size( c );
+szCs = szC;
+szCs(dim) = 1;
+cs = cell(szCs);
+
+targetIdxVectors = {};
+for targetIdx = 1 : numel( szC )
+    if targetIdx == dim
+        targetIdxVectors{end+1} = ones( 1, szC(targetIdx) );
+    else
+        targetIdxVectors{end+1} = 1 : szC(targetIdx);
+    end
+end
+
+targetIdxGrid = cell( 1, numel( szC ) );
+[targetIdxGrid{:}] = ndgrid( targetIdxVectors{:} );
+
+for cLinIdx = 1 : numel( c )
+    targetIdx = {};
+    for kk = 1 : numel( targetIdxGrid )
+        targetIdx{end+1} = targetIdxGrid{kk}(cLinIdx);
+    end
+    if isempty( cs{targetIdx{:}} )
+        cs{targetIdx{:}} = c(cLinIdx);
+    else
+        cs{targetIdx{:}} = cat( dim, cs{targetIdx{:}}, c{cLinIdx} );
+    end
+end
+
+cs = squeeze( cs );
+cs = cellfun( fn, cs );
