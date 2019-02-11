e6a963d7797a4ed968a5270748365f9bb16a3679
ivo trowitzsch <ivo.trowitzsch@gmail.com>
2019-02-11 16:56:00 +0100

    fix in getMFilePath.m

--- Tools/misc/getMFilePath.m
+++ Tools/misc/getMFilePath.m
@@ -1,6 +1,9 @@
 function mpath = getMFilePath()
 
 ST = dbstack( 1, '-completenames' );
+if isempty( ST )
+    error( 'Function only works if called from an m-file.' );
+end
 mFileName = strtok( ST(1).name, '.' );
 mFilePath = ST(1).file;
 nIgnoreChars = length( mFileName ) + 3;
